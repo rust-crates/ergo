a08843a33e2f4d2eb064b38b2c4c61e66dc3ac31
Garrett Berg <googberg@gmail.com>
2018-02-03 22:50:00 -0700

    add license and boilerplate

---
+++ README.md
@@ -0,0 +1,21 @@
+# ergo_std: items that could be in the standard library.
+
+[![Build Status](https://travis-ci.org/vitiral/ergo_std.svg?branch=windows)](https://travis-ci.org/vitiral/ergo_std)
+[![Build status](https://ci.appveyor.com/api/projects/status/vgis54solhygre0n?svg=true)](https://ci.appveyor.com/project/vitiral/path-abs)
+[![Docs](https://docs.rs/ergo_std/badge.svg)](https://docs.rs/ergo_std)
+
+**See the [library docs](https://docs.rs/ergo_std) for more information**
+
+
+# LICENSE
+The source code in this repository is Licensed under either of
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  http://opensource.org/licenses/MIT)
+
+at your option.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,64 +1,79 @@
-// -------------------------------------
-// ----------- CUSTOM MACROS -----------
+//! **ergo_std**: items that could be in the standard library.
+//!
+//! This is the "core types" library as part of the [`ergo`] crates ecosystem. It contains useful
+//! types, traits and functions for general purpose programming projects which do not fall
+//! into the other [`ergo`] crates but which are boons to ergonomics and productivity.
+//!
+//! # How to Use
+//!
+//! ```rust
+//! #[macro_use] extern crate failure;
+//! extern crate serde;
+//! #[macro_use] extern crate serde_derive;
+//! #[macro_use] extern crate ergo_std;
+//! use ergo_std::*;
+//! # fn main() {
+//! # }
+//! ```
+//!
+//! > _As you notice, this crate does not include `serde` or `failure`. This is due to a bug
+//! > which makes it impossible for this crate to rexport their `#[derive(...)]` macros.
+//!
+//! # Exported Items
+//!
+//! The following crates and types are exported. See their docs for how to use them.
+//!
+//! - **[`std_prelude`]**: extends rust's additional prelude with commonly used types. The
+//!   crate is well documented with justification and usecases for each type.
+//! - **[`lazy_static!`]**: the `lazy_static!` macro is the current standard way to create
+//!   global variables and constants in a majority of crates.
+//! - **[`itertools`]**: the itertools prelude provides traits that extends rust's already
+//!   extensive iterator API.
+//! - **[`maplit`]**: provides `hashmap!`, `hashset!`, `btreemap!` and `btreeset!` macros to
+//!   compliment rust's existing `vec!` macro. These
+//! - **[`Regex`]**: the regular expression type from the `regex` crate.
+//!
+//! [`ergo`]: https://github.com/rust-crates/ergo
+//! [`std_prelude`]: ../std_prelude/index.html
+//! [`itertools`]: ../itertools/index.html
+//! [`lazy_static!`]: ../lazy_static/index.html
+//! [`maplit`]: ../maplit/index.html
+//! [`Regex`]: struct.Regex.html
+//!
+//! ### Special thanks
+//!
+//! The crates that are exported are:
+//!
+//! - [**std_prelude**](https://github.com/vitiral/std_prelude):
+//!   Multi-producer multi-consumer channels for message passing
+//! - [**lazy_static**](TODO): TODO
+//! - [**itertools**](TODO): TODO
+//! - [**maplit**](TODO): TODO
+//! - [**regex**](TODO): TODO
+//!
+//! Consider supporting their development individually and starring them on github.
+//!
+//! ## Future crates
+//!
+//! The following crates will be added in the future:
+//!
+//! - `indexmap`: the current crate is `ordermap`, which is renaming itself
+//!   `indexmap` and changing what `ordermap` is... it's confusing but it
+//!   will be comming shortly
 
-#[macro_export]
-/// Use to take mutable ownership of _specific_ variables within a closure.
-///
-/// Closures try to be "smart" about how much scope they capture. If you don't mutate a variable
-/// they take `&var`, if you do mutate they take `&mut var`. However, if you require ownership you use
-/// `move`, i.e. `move || {... do stuff with var...}`... _right_?
-///
-/// The problem with `move` is that it moves _every_ variable that is referenced. If you only need
-/// to move _one_ variable it can be a pain. Interestingly, you can tell the compiler to only move
-/// _specific_ variables like so:
-///
-/// ```no_compile
-/// let x = x;
-/// let y = y;
-/// // ... etc
-/// ```
-///
-/// But this is quite silly and not obvious to someone who doesn't know about it. Instead, use the
-/// `own!` macro and your code will be self documenting.
-///
-/// > Note: this macro always does `let mut x = x` to mimick its primary usecase of closures
-/// > (which infer mutability automatically). If you require non-mutable ownership use `let x = x`
-/// > directly.
-///
-/// # Examples
-/// ```
-/// #[macro_use] extern crate std_prelude;
-///
-/// # fn main() {
-/// let y = vec![1, 2, 3];
-/// let mut x = vec![1, 2, 3];
-/// let z = vec![10];
-///
-/// // create scope in which we mutate `x`
-/// {
-///     let closure = || {
-///         own!(y, z);
-///         x.push(4); // mutate reference to x
-///         z.push(10);
-///
-///         println!("&x: {:?}", &x);
-///         println!("moved y: {:?}", y);
-///         println!("moved z: {:?}", z);
-///     };
-///
-///     closure();
-/// }
-///
-/// println!("&x after: {:?}", x);    // We can still print x!
-/// // println!("y after: {:?}", y);  // ERROR: use of moved value
-/// // println!("z after: {:?}", z);  // ERROR: use of moved value
-/// # }
-/// ```
-macro_rules! own {
-    ( $( $x:ident ),* ) => {
-        #[allow(unused_mut)]
-        $(
-            let mut $x = $x;
-        )*
-    };
-}
+#[macro_use]
+pub extern crate itertools;
+#[macro_use]
+pub extern crate lazy_static;
+#[macro_use]
+pub extern crate maplit;
+pub extern crate std_prelude;
+pub extern crate regex;
+
+pub use std_prelude::*;
+pub use itertools::prelude::*;
+pub use lazy_static::*;
+pub use itertools::Itertools;
+pub use maplit::*;
+pub use regex::Regex;
+
