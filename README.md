004ea79cafbe48476159e3d7da2891775784a839
Garrett Berg <googberg@gmail.com>
2018-02-03 23:10:06 -0700

    add boilerplate

---
+++ README.md
@@ -0,0 +1,21 @@
+# ergo_config: loading configuration more ergonomic, therefore fun!
+
+[![Build Status](https://travis-ci.org/vitiral/ergo_config.svg?branch=windows)](https://travis-ci.org/vitiral/ergo_config)
+[![Build status](https://ci.appveyor.com/api/projects/status/vgis54solhygre0n?svg=true)](https://ci.appveyor.com/project/vitiral/path-abs)
+[![Docs](https://docs.rs/ergo_config/badge.svg)](https://docs.rs/ergo_config)
+
+**See the [library docs](https://docs.rs/ergo_config) for more information**
+
+
+# LICENSE
+The source code in this repository is Licensed under either of
+- Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or
+  http://www.apache.org/licenses/LICENSE-2.0)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or
+  http://opensource.org/licenses/MIT)
+
+at your option.
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall
+be dual licensed as above, without any additional terms or conditions.
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,33 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+//! **Make loading configuration more ergonomic, therefore fun!**
+//!
+//! This is the configuration loading libray as part of the [`ergo`] crate ecosystem.
+//!
+//! > This library should rarely be used on its own. Refer to the [`ergo`] crate ecosystem
+//! > for how to use its exported features. For one thing it depends on `serde` to actually
+//! > be used in most cases.
+//!
+//! ### Special thanks
+//!
+//! The crates that are exported are:
+//!
+//! - [**configure**](https://github.com/withoutboats/configure): pull in configuration from the
+//!   environment.
+//! - [**ron**](https://github.com/ron-rs/ron): Rusty Object Notation.
+//! - [**serde_json**](https://github.com/serde-rs/json): Strongly typed JSON library for Rust.
+//! - [**serde_yaml**](https://github.com/dtolnay/serde-yaml): Strongly typed YAML library for
+//!   Rust.
+//! - [**toml**](https://github.com/alexcrichton/toml-rs): A TOML encoding/decoding library for
+//!   Rust.
+//!
+//! Consider supporting their development individually and starring them on github.
+//!
+//! [`ergo`]: https://github.com/rust-crates/ergo
+#![allow(unused_imports)]
+
+#[macro_use]
+pub extern crate configure;
+pub extern crate serde_json as json;
+pub extern crate serde_yaml as yaml;
+pub extern crate toml;
+
+pub use configure::*;
